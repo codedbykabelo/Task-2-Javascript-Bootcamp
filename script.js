fafae473d22ac22eb8e05aa64333d1aa0fb4838e
Mpashi Makgabo <mpashi365@gamil.com>
2026-05-31 19:27:48 +0200

    Added input value retrieval

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function checkMark() {
 
+    function checkMark() {
+    let studentName = document.getElementById("studentName").value;
+    let studentMark = parseInt(document.getElementById("studentMark").value);
+}
 }
